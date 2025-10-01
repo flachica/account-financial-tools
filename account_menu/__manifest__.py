258a4fa17e067a48bd9890a2a64e77c08b2bcc25
Fernando La Chica (Sidoo SDi. Digital Group) <flachica@sidoo.es>
2025-10-01 09:50:39 +0000

    [MIG] account_menu: Migration to 18.0

--- account_menu/__manifest__.py
+++ account_menu/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account - Missing Menus",
-    "version": "14.0.1.2.2",
+    "version": "18.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Adds missing menu entries for Account module",
